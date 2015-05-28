795b79f265d68b6eb00ee32132a774bc55672734
yusuke Yama <ymc9.knit.p23@gmail.com>
2015-05-28 23:50:28 +0900

    新しく作った

--- database/migrations/2015_05_27_153729_create_user_table.php
+++ database/migrations/2015_05_27_153729_create_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableOpponent extends Migration {
+class CreateUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -13,11 +13,6 @@ class CreateTableOpponent extends Migration {
 	public function up()
 	{
 		//
-		Schema::create('opponent',function($table){
-			$table->integer('opponent_id');
-			$table->string('opponent_name');
-			$table->primary('opponent_id');
-		});
 	}
 
 	/**
@@ -28,7 +23,6 @@ class CreateTableOpponent extends Migration {
 	public function down()
 	{
 		//
-		Schema::dropIfExists('opponent');
 	}
 
 }
